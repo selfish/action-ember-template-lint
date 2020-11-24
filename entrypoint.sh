5410927e5b810ac85e54d23a01d06bcf73fd7e48
Tal Tchernihovski <taltcher@gmail.com>
2020-11-24 17:33:49 +0200

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,11 @@ fi
 
 $(npm bin)/ember-template-lint --version
 
-$(npm bin)/ember-template-lint ${INPUT_TEMPLATE_LINT_FLAGS:-'.'} \
-  | reviewdog -f=rdjson \
+OUT=$($(npm bin)/ember-template-lint --json ${INPUT_TEMPLATE_LINT_FLAGS:-'.'})
+echo "$OUT"
+echo "$OUT"  | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
