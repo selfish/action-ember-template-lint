a65e7b3d2bb68eb701e5f409add7afecac043165
Tal Tchernihovski <taltcher@gmail.com>
2020-11-25 14:28:56 +0200

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,12 +12,6 @@ fi
 
 "$(npm bin)"/ember-template-lint --json ${INPUT_TEMPLATE_LINT_FLAGS:-'.'} > out.json
 
-cat out.json
-echo "=========="
-echo "=========="
-echo "=========="
-echo "=========="
-
 cat out.json | sed '/^#/d' | node /formatter.js | reviewdog -f=rdjson \
       -name="${INPUT_TOOL_NAME}" \
       -reporter="${INPUT_REPORTER:-github-pr-review}" \
