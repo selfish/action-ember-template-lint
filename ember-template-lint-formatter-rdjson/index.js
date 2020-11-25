5b72ee271b0023b4edf4c90426f437dc2fadbb5c
Nitai J. Perez <nitai@tailorbrands.com>
2020-11-25 16:37:59 +0200

    Moar fixes

--- ember-template-lint-formatter-rdjson/index.js
+++ ember-template-lint-formatter-rdjson/index.js
@@ -19,36 +19,36 @@ function utf8length(str) {
 
 function positionFromUTF16CodeUnitOffset(offset, text) {
   const lines = text.split('\n');
-  let lnum = 1;
+  let line = 1;
   let column = 0;
   let lengthSoFar = 0;
-  for (const line of lines) {
-    if (offset <= lengthSoFar + line.length) {
-      const lineText = line.slice(0, offset-lengthSoFar);
+  for (const sourceLine of lines) {
+    if (offset <= lengthSoFar + sourceLine.length) {
+      const lineText = sourceLine.slice(0, offset - lengthSoFar);
       // +1 because eslint offset is a bit weird and will append text right
       // after the offset.
       column = utf8length(lineText) + 1;
       break;
     }
-    lengthSoFar += line.length + 1; // +1 for line-break.
-    lnum++;
+    lengthSoFar += sourceLine.length + 1; // +1 for line-break.
+    line++;
   }
-  return {line: lnum, column: column};
+  return { line, column };
 }
 
-function positionFromLineAndUTF16CodeUnitOffsetColumn(line, column, sourceLines) {
-  let col = 0;
+function positionFromLineAndUTF16CodeUnitOffsetColumn(line, sourceColumn, sourceLines) {
+  let column = 0;
   if (sourceLines.length >= line) {
-    const lineText = sourceLines[line-1].slice(0, column);
-    col = utf8length(lineText);
+    const lineText = sourceLines[line - 1].slice(0, sourceColumn);
+    column = utf8length(lineText);
   }
-  return {line: line, column: col};
+  return { line, column };
 }
 
 function commonSuffixLength(str1, str2) {
   let i;
   for (i = 0; i < str1.length && i < str2.length; ++i) {
-    if (str1[str1.length-(i+1)] !== str2[str2.length-(i+1)]) {
+    if (str1[str1.length - (i + 1)] !== str2[str2.length - (i + 1)]) {
       break;
     }
   }
@@ -56,7 +56,7 @@ function commonSuffixLength(str1, str2) {
 }
 
 function buildMinimumSuggestion(result) {
-  const {fix, line, source} = result;
+  const { fix, line, source } = result;
   const l = commonSuffixLength(fix.text, source.slice(line, line));
   return {
     range: {
@@ -67,65 +67,56 @@ function buildMinimumSuggestion(result) {
   };
 }
 
-function buildRdJsonOutput(results, data) {
-  const rdjson = {
-    source: {
-      name: 'ember-template-lint',
-      url: 'https://eslint.org/'
+function formatDiagnostic(diagnostic) {
+
+  const {filePath, source} = diagnostic;
+  const sourceLines = source ? source.split('\n') : [];
+
+  let formattedDiagnostic = {
+    message: diagnostic.message,
+    location: {
+      path: filePath,
+      range: {
+        start: positionFromLineAndUTF16CodeUnitOffsetColumn(diagnostic.line, diagnostic.column, sourceLines),
+        end: positionFromLineAndUTF16CodeUnitOffsetColumn(diagnostic.line, diagnostic.column, sourceLines)
+      }
     },
-    diagnostics: []
+    severity: convertSeverity(diagnostic.severity),
+    code: {
+      value: diagnostic.rule,
+      url: `https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/${diagnostic.rule}.md` //(data.rulesMeta[msg.rule] && data.rulesMeta[msg.rule].docs ? data.rulesMeta[msg.rule].docs.url : '')
+    },
+    original_output: JSON.stringify(diagnostic)
   };
 
-  Object.keys(results).forEach(file => {
-    results[file].forEach(result => {
-      const filePath = result.filePath;
-      const source = result.source;
-      const sourceLines = source ? source.split('\n') : [];
+  if (formattedDiagnostic.fix) {
+    formattedDiagnostic.suggestions = [buildMinimumSuggestion(formattedDiagnostic)];
+  }
 
-      let diagnostic = {
-        message: result.message,
-        location: {
-          path: filePath,
-          range: {
-            start: positionFromLineAndUTF16CodeUnitOffsetColumn(result.line, result.column, sourceLines),
-            end:positionFromLineAndUTF16CodeUnitOffsetColumn(result.line, result.column, sourceLines)
-          }
-        },
-        severity: convertSeverity(result.severity),
-        code: {
-          value: result.rule,
-          url: `https://github.com/ember-template-lint/ember-template-lint/blob/master/docs/rule/${result.rule}.md` //(data.rulesMeta[msg.rule] && data.rulesMeta[msg.rule].docs ? data.rulesMeta[msg.rule].docs.url : '')
-        },
-        original_output: JSON.stringify(result)
-      };
+  return formattedDiagnostic;
+}
 
-      if (result.fix) {
-        diagnostic.suggestions = [buildMinimumSuggestion(result)];
-      }
+function buildRdJsonOutput(results) {
+  return {
+    source: {
+      name: 'ember-template-lint',
+      url: 'https://github.com/ember-template-lint/ember-template-lint'
+    },
+    diagnostics: Object.values(results).flat().map(formatDiagnostic)
+  };
+}
 
-      rdjson.diagnostics.push(diagnostic);
-    });
-  })
+let data = '';
 
-  return JSON.stringify(rdjson);
-};
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 
-const data = require('./test.json');
+process.stdin.on('data', function(chunk) {
+  data += chunk;
+});
 
-// let data = '';
-//
-// process.stdin.resume();
-// process.stdin.setEncoding('utf8');
-//
-// process.stdin.on('data', function(chunk) {
-//   data += chunk;
-// });
-//
-// process.stdin.on('end', function() {
-//   const parsed = JSON.parse(data);
-//   const rdJson = buildRdJsonOutput(parsed);
-//   console.log(rdJson);
-// });
-
-const rdJson = buildRdJsonOutput(data);
-console.log(rdJson);
+process.stdin.on('end', function() {
+  const parsed = JSON.parse(data);
+  const rdJson = buildRdJsonOutput(parsed);
+  console.log(JSON.stringify(rdJson));
+});
--- template-lintrc.js
+++ template-lintrc.js
@@ -1,3 +1,5 @@
 'use strict';
 
-module.exports = { extends: 'octane' };
+module.exports = {
+  extends: ['recommended', 'octane', 'stylistic']
+};
